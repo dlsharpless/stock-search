df90ce44478ddfec9f2d5f297a5e1caf73942265
dlsharpless <10237785+dlsharpless@users.noreply.github.com>
2018-10-02 20:03:34 -0500

    added validationList and styling

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
 let stockList = ["FB","AAPL","TSLA","GOOG"];
 let validationList = [];
 
-let displayStockInfo = function () {
-    let stock = $(this).attr("symbol");
-    let queryURL = `https://api.iextrading.com/1.0/stock/${stock}/batch?types=quote,news&range=1m&last=1`;
-
+let getListedSecurities = function () {
+    let rawList = [];
+    let queryURL = `https://api.iextrading.com/1.0/ref-data/symbols`;
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        let stockDiv = $("<div>").addClass("stock");
-        let name = $("<p>").text(`Company Name: ${response.quote.companyName}`);
-        let symbol = $("<p>").text(`Stock Symbol: ${response.quote.symbol}`);
-        let price = $("<p>").text(`Stock Price: ${response.quote.latestPrice}`);
-        let news = $("<p>").text(`News Headline: ${response.news[0].summary}`);
-        stockDiv.append(name,symbol,price,news);
-        $("#stockInfo").prepend(stockDiv);
+        rawList = response;
+        for (i = 0; i < rawList.length; i++) {
+            validationList.push(rawList[i].symbol);
+        }
     })
 }
 
-let render = function () {
+let displayStockInfo = function () {
+    let stock = $(this).attr("symbol");
+    let queryURL = `https://api.iextrading.com/1.0/stock/${stock}/batch?types=quote,news&range=1m&last=3`;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        let stockRow = $("<tr>").addClass("stock");
+        let name = $("<td>").text(response.quote.companyName);
+        let symbol = $("<td>").text(response.quote.symbol);
+        let price = $("<td>").text(response.quote.latestPrice);
+        let news = $("<td>");
+        for (j = 0; j < 3; j++) {
+            let story = `<a href="${response.news[j].url}">${response.news[j].headline}</a>`;
+            news.append(story);
+            if (j < 2) {
+                news.append("<br>");
+            }
+        }
+        stockRow.append(name,symbol,price,news);
+        $("#stockInfo").prepend(stockRow);
+    })
+}
+
+let renderButtons = function () {
     $("#stockButtons").empty();
     for (let i = 0; i < stockList.length; i++) {
         let newButton = $("<button>").addClass("stockButton");
@@ -31,14 +51,15 @@ let render = function () {
 
 let addButton = function(event) {
     event.preventDefault();
-    let inquiry = $("#stockInput").val().trim();
-    if (validationList.includes(inquiry)) {
+    let inquiry = $("#stockInput").val().trim().toUpperCase();
+    if (validationList.includes(inquiry) &! stockList.includes(inquiry)) {
         stockList.push(inquiry);
         $("#stockInput").val("");
     }
-    render();
+    renderButtons();
 }
 
 $("#addStock").on("click", addButton);
 $("#stockButtons").on("click", ".stockButton", displayStockInfo);
-render();
+getListedSecurities();
+renderButtons();
